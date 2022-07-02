8ac6dc0e476d7df7e4ee0fba97db93b388360fad
chibatakashi <nixon0290211@icloud.com>
2022-07-02 11:00:54 +0900

    first commit

---
+++ 01.py
@@ -0,0 +1,16 @@
+from cgitb import html
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+
+
+
+
+
+    if __name__ == "__main__":
+    app.run(debug=True)
